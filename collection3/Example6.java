7231fa902a791d64ed015688b98f7da81c4d651f
Ramalingareddy1 Chintakunta <ramareddyc1996@gmail.com>
2023-03-11 18:24:47 +0000

    Checking hashmap Values or exist or not

---
+++ collection3/Example6.java
@@ -0,0 +1,34 @@
+package ref_dev.collection3;
+import java.util.*;
+
+public class Example6 {
+    public static void main(String[] args) {
+        HashMap < Integer, String > hash_map = new HashMap < Integer, String > ();
+        hash_map.put(1, "Red");
+        hash_map.put(2, "Green");
+        hash_map.put(3, "Black");
+        hash_map.put(4, "White");
+        hash_map.put(5, "Blue");
+
+        System.out.println("The Original map: " + hash_map);
+        System.out.println("1. Is value \'Green\' exists?");
+
+        if(hash_map.containsValue("Green")){
+            System.out.println("Yes...");
+
+        }
+        else {
+            System.out.println("No...");
+        }
+        System.out.println("\n2. Is value \'Grey\' exists?");
+        if (hash_map.containsValue("Grey")){
+            System.out.println("Yes..");
+        }
+        else {
+            System.out.println("No");
+        }
+
+
+    }
+    
+}
