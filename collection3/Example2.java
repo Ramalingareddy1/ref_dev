6ddf05fa7041c7e3a776fc125a9b28af28cad27e
Ramalingareddy1 Chintakunta <ramareddyc1996@gmail.com>
2023-03-11 16:58:57 +0000

    Adding size of Hashmap

---
+++ collection3/Example2.java
@@ -0,0 +1,20 @@
+package ref_dev.collection3;
+
+import java.util.HashMap;
+
+public class Example2 {
+    public static void main(String[] args) {
+        
+    
+    HashMap<Integer,String> hm= new HashMap<Integer,String>();
+    hm.put(1,"White");
+    hm.put(2, "Red");
+    hm.put(3,"Orange");
+    hm.put(4,"Black");
+    hm.put(5,"Blue");
+    System.out.println(hm);
+    System.out.println(" Size of the hasmap:"+hm.size());
+
+    }
+    
+}
